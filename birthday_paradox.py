c9701dde99f35f7696ab416da3d3066730150827
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-03-28 15:16:59 -0400

    Create birthday_paradox.py
    creating birthday paradox program

---
+++ birthday_paradox.py
@@ -0,0 +1,8 @@
+#Birthday paradox
+"""Birthday Paradox by Christian Brewer
+This program shows the likelihood of two people sharing the same birthday in a group of people. """
+
+def main():
+  num_people = input("How many birthdays shall I generate?: ")
+  
+
