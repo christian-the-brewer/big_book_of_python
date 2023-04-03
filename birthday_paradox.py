9d77e2c5966c37ec69bcbd59c765dd674935f87f
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-04-03 15:54:31 -0400

    Update birthday_paradox.py
    add function to print birthdays

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -4,7 +4,7 @@ This program shows the likelihood of two people sharing the same birthday in a g
 import random
 
 #days in months
-  days_in_month = {"January": 31,
+days_in_month = {"January": 31,
   "February": 28,
   "March": 31,
   "April": 30,
@@ -22,12 +22,17 @@ months = ["January", "February", "March", "April", "May", "June", "July", "Augus
 
 def main():
   #get sample size
-  num_people = input("How many birthdays shall I generate? (Max 100): ")
+  num_people = int(input("How many birthdays shall I generate? (Max 100): "))
   #show list
   print(f"Here are {num_people} birthdays:")
   
   #for each person, generate a month and day
-
+  birthdays = get_birthdays(num_people)
+  #iterate over list of birthdays and print out
+  for birthday in birthdays:
+    print(birthday, end=", ")
+  print(get_matches(birthdays))
+  
 
 
 
@@ -38,4 +43,13 @@ def get_birthdays(number):
     month = random.choice(months)
     day = random.randrange(1, days_in_month[month])
     birthday = f"{month} {day}"
-    
+    birthday_list.append(birthday)
+  return birthday_list
+
+def get_matches(birthdays):
+  matches = set()
+  shared_birthdays = {birthday for birthday in birthdays if birthday in matches or (matches.add(birthday) or False)}
+  return matches
+
+  
+main()
