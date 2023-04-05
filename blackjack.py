43b9fcfe6bd0a0f219cb9a9c66e5387796d8e1f6
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-04-05 15:41:06 -0400

    Update blackjack.py
    add constants

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,30 @@
 #blackjack by Christian Brewer
+
+import random, sys
+
+#Constants
+HEARTS = chr(9829)
+DIAMONDS = chr(9830)
+SPADES = chr(9824)
+CLUBS = chr(9827)
+BACKSIDE = "backside"
+
+def main():
+  print("""Rules:
+       Try to get as close to 21 without going over.
+       Kings, Queens, and Jacks are worth 10 points.
+       Aces are worth 1 or 11 points.
+       Cards 2 through 10 are worth their face value.
+       (H)it to take another card.
+       (S)tand to stop taking cards.
+       On your first play, you can (D)ouble down to increase your bet
+       but must hit exactly one more time before standing.
+       In case of a tie, the bet is returned to the player.
+       The dealer stops hitting at 17.""")
+
+  money = 5000
+
+  while True: #game loop
+    #check if player has money
+    if money <= 0:
+      print("You are broke!")
