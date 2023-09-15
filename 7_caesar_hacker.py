090ea86fa965177091085c847219b6bfa8116231
Christian <brewer.christiang@gmail.com>
2023-09-14 20:41:31 -0400

    add python file extension

--- 7_caesar_hacker.py
+++ 7_caesar_hacker.py
@@ -14,4 +14,18 @@ for key in range(len(SYMBOLS)):
 
   #decrypt each symbol in message
   for symbol in message:
-    if symbol in SYMb
+    if symbol in SYMBOLS:
+      num = SYMBOLS.find(symbol) #find number of symbol
+      num = num - key
+      
+      #handle wrap around
+      if num < 0:
+        num = num + len(SYMBOLS)
+      
+      #add to translated
+      translated = translated + SYMBOLS[num]
+      
+    else:
+      translated = translated + symbol
+      
+  print(f'Key #{key}: {translated}')
