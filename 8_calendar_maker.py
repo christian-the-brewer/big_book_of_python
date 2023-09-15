2bad747938249483a1235f7561b4747af0f4539b
Christian <brewer.christiang@gmail.com>
2023-09-15 14:50:39 -0400

    creates calendar maker

---
+++ 8_calendar_maker.py
@@ -0,0 +1,51 @@
+#https://inventwithpython.com/bigbookpython/project8.html
+"""Calendar Maker by Christian Brewer"""
+
+import datetime
+
+#constants
+DAYS = (
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+)
+MONTHS = (
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+)
+
+print('---Calendar Maker---')
+
+while True:
+    print('Enter year: ')
+    response = input('> ')
+    
+    if response.isdecimal() and int(response) > 0:
+        year = int(response)
+        break
+    print('Please enter a year (2023)')
+    continue
+
+while True:
+    print('Enter month (1-12): ')
+    response = input('> ')
+    
+    if not response.isdecimal():
+        print('Please enter a numerical month (1-12):')
+        continue
+    
+    month = int(response)
+    if 1 <= month <= 12:
+        break
+    
+    print('Please enter a number from 1 - 12.')
+    continue
+
+def get_calendar(year, month):
+    cal_text = '' #string representation of calendar
+    
+    #print month and year at top of string
+    cal_text += f"{' ' * 34}{MONTHS[month - 1]} {str(year)}\n"
+    #add days of week
+    for day in DAYS:
+        i = 0
+        cal_text += f"...{DAYS[i]}"
+    cal_text += '..\n'
+    
